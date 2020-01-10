70b70ef2c33c1963540ebbd9ab4a75518b68a3dd
Conor ONeill <cwjoneill@gmail.com>
2020-01-10 17:26:28 +0000

    added multi switch support. If any switch is on, light is on

--- light_esp32.js
+++ light_esp32.js
@@ -25,43 +25,75 @@ BluetoothDevice: {
  
  */
 
+/*
+This version allows for multiple switches to be visible to one light.
+If *any* switch is on, the light is on.
+All switches must be off for light to be off
+*/
+
 var disp;
 var text;
 var x;
 var g;
 var intervalID;
-var currState;
-var nextState;
+var currState = 0;
+var nextState = 0;
+
+var switchUpdates = [];
 
 
 function scanForDevices() {
-    NRF.findDevices(function(devs) {
-      var idx = 0;
-      devs.forEach(function(dev) {
-        if (dev.name!="Im On A Call Switch") return;
-     //   console.log(dev);
-        if (!dev.manufacturerData) return;
-        var d = new DataView(dev.manufacturerData);
-        nextState = (d.getInt8(0));
-        idx++;
-        if (currState === 0 && nextState === 1){
-            intervalID = setInterval(scroll, 5);
-            currState = 1;
-        } else if (currState === 1 && nextState === 0){
-            clearInterval(intervalID);
-            currState = 0;
-            x=0;
-            g.clear();
-            g.flip();
-        }
-      });
-    //  if (!idx) console.log("(no devices found)");
-    }, 1000); // scan for 1 secs
+  NRF.findDevices(function (devs) {
+    devs.forEach(function (dev) {
+      if (dev.name != "Im On A Call Switch") return;
+      //   console.log(dev);
+      if (!dev.manufacturerData) return;
+      var d = new DataView(dev.manufacturerData);
+      buttonState = (d.getInt8(0));
+
+      var update = {
+        switch: dev.id,
+        state: buttonState
+      };
+
+      // See if device already seen
+      var switchIndex = switchUpdates.findIndex(o => o.switch === dev.id);
+
+      // If not, add device to array
+      if (switchIndex === -1) {
+        switchIndex = switchUpdates.push(update) - 1;
+        //  console.log("Added device " + switchIndex + " to list: " + update.switch);
+      } else {
+        // Update State
+        //  console.log("Updated device " + update.switch + " to: " + update.state);
+        switchUpdates[switchIndex].state = buttonState;
+      }
+    });
+  }, 2000); // scan for 2 secs
+
+  // Now do a loop on all entries and see if any are set to 1
+  nextState = 0;
+  for (var i = 0; i < switchUpdates.length; i++) {
+    if (switchUpdates[i].state == 1) nextState = 1;
   }
-  
- 
-function scroll() {x--;
-  if (x<-g.stringWidth(text)) x=g.getWidth();
+  // If any set to one, turn on display
+  if (currState === 0 && nextState === 1) {
+    intervalID = setInterval(scroll, 5);
+    currState = 1;
+    // If none set to 1, turn off display    
+  } else if (currState === 1 && nextState === 0) {
+    clearInterval(intervalID);
+    currState = 0;
+    x = 0;
+    g.clear();
+    g.flip();
+  }
+}
+
+
+function scroll() {
+  x--;
+  if (x < -g.stringWidth(text)) x = g.getWidth();
   g.clear();
   g.drawString(text, x, 0);
   g.flip();
@@ -69,16 +101,16 @@ function scroll() {x--;
 
 function onInit() {
   require("FontSinclair").add(Graphics);
-  SPI1.setup({mosi:D23, sck:D18});
+  SPI1.setup({ mosi: D23, sck: D18 });
   disp = require("MAX7219").connect(SPI1, D5, 4 /* 4 chained devices */);
   text = "Conor is on a call.";
   x = 0;
   g = Graphics.createArrayBuffer(32, 8, 1);
-  currState=0;
-  nextState=0;
-  g.flip = function() { disp.raw(g.buffer); }; // To send to the display
+  currState = 0;
+  nextState = 0;
+  g.flip = function () { disp.raw(g.buffer); }; // To send to the display
   setInterval(scanForDevices, 2000); // update every 2 seconds
-  scanForDevices();  
+  scanForDevices();
 }
 
 
